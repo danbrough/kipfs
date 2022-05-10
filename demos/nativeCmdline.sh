430ea5482bb4bdc568f9bd5db5efafad11758296
Dan Brough <dan@danbrough.org>
2022-05-11 08:22:37 +1200

    update

--- demos/nativeCmdline.sh
+++ demos/nativeCmdline.sh
@@ -3,4 +3,10 @@
 cd $(dirname $0) && cd ..
 export LD_LIBRARY_PATH=$(realpath golib/build/native/linuxAmd64)
 
-exec ./gradlew demos:native_cmdline:runKipfsDemoDebugExecutableLinuxAmd64 $@
+if [ -z "$1" ]; then
+  IPFS_ADDR=/ip4/127.0.0.1/tcp/5001
+else
+  IPFS_ADDR=$1
+fi
+
+exec ./gradlew demos:native_cmdline:runKipfsDemoDebugExecutableLinuxAmd64 -Pargs=$IPFS_ADDR
