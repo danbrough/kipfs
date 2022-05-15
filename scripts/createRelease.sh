0f8b94f37bd08f0337d52e786a0f97edbfd09841
Dan Brough <dan@danbrough.org>
2022-05-15 17:19:06 +1200

    release: 0.0.1-alpha16

--- scripts/createRelease.sh
+++ scripts/createRelease.sh
@@ -7,10 +7,22 @@ cd `dirname $0` && cd ..
 
 VERSION=$(./gradlew -q versionName)
 
+echo creating release with version $VERSION
+sleep 5
+
+
 echo commiting version $VERSION
 
 git add .
 git commit -am "release: $VERSION"
 git tag $VERSION || exit 1
+git push
+git push origin $VERSION
+
+syncmaven 2> /dev/null
+
+./gradlew -q versionIncrement
+
+
 
 
