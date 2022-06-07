a5a856fa115c79226ca36e19575fd74dd9198fae
Dan Brough <dan@danbrough.org>
2022-06-08 08:24:52 +1200

    update

--- buildSrc/src/main/kotlin/TEsts.kt
+++
@@ -1,7 +0,0 @@
-import org.jetbrains.kotlin.gradle.dsl.*
-import org.jetbrains.kotlin.gradle.plugin.mpp.*
-
-
-/*
-fun KotlinMultiplatformExtension.linuxTargets(): Set<KotlinNativeTarget> =
-  linuxArm64() + linuxX64()*/
