ccd52b1b6be385a73222ad28ec4d38a1636f1567
Dan Brough <dan@danbrough.org>
2022-08-04 16:06:03 +1200

    update

--- golib/src/nativeTest/kotlin/org/danbrough/kipfs/NativeTests.kt
+++ golib/src/nativeTest/kotlin/org/danbrough/kipfs/NativeTests.kt
@@ -6,14 +6,20 @@ import kotlinx.cinterop.cstr
 import kotlinx.cinterop.readBytes
 import kotlinx.cinterop.toCValues
 import kotlinx.cinterop.useContents
-import org.danbrough.klog.*
+import klog.*
 import kotlin.test.Test
 
 class NativeTests {
-
-  private val log = klog(level = Level.TRACE, formatter = KLogFormatters.verbose.colored, writer = KLogWriters.stdOut)
-
-
+  
+  companion object {
+    private val log = klog {
+      level = Level.TRACE
+      messageFormatter = KMessageFormatters.verbose.colored
+      writer = KLogWriters.stdOut
+    }
+  }
+  
+  
   private fun multibaseEncode(encoding: MultibaseEncoding, data: ByteArray): String =
     kipfs.KMultiBaseEncode(encoding.encoding.code, data.toCValues(), data.size).useContents {
       r1?.also {
@@ -21,8 +27,8 @@ class NativeTests {
       }
       r0!!.copyToKString()
     }
-
-
+  
+  
   private fun multibaseDecode(data: String): MultibaseDecodeResult =
     kipfs.KMultiBaseDecode(data.cstr, data.length).useContents {
       r3?.copyToKString()?.also {
@@ -30,19 +36,19 @@ class NativeTests {
       }
       MultibaseDecodeResult(MultibaseEncoding.valueOf(r0.toInt()), r1!!.readBytes(r2))
     }
-
-
+  
+  
   @Test
   fun test1() {
     val encoding = MultibaseEncoding.Base64url
     listOf("testing", "123", "\u0000\u0001\u0002").forEach {
       val result = multibaseEncode(encoding, it.encodeToByteArray())
       log.info("$it => $result")
-
+      
       val decoded = multibaseDecode(result)
       log.debug("decoded: <$decoded>")
-
+      
     }
   }
-
+  
 }
