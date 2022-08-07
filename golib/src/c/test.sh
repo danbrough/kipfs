889828ab8d8e1c1aa61c7d53083caf3115aa342b
Dan Brough <dan@danbrough.org>
2022-08-08 10:56:00 +1200

    update

--- golib/src/c/test.sh
+++ golib/src/c/test.sh
@@ -2,9 +2,13 @@
 
 cd $(dirname "$0")
 
-LIBDIR=../../build/lib/linuxX64/
+if [ "$MACH_TYPE" = "x86_64-apple-darwin21" ]; then
+  LIBDIR="$(realpath ../../build/lib/macosX64/)"
+else
+  LIBDIR="$(realpath ../../build/lib/linuxX64/)"
+fi
 
 export LD_LIBRARY_PATH="$LIBDIR"
 
-gcc -o test test.c -lkipfsgo -L $LIBDIR -I $LIBDIR -I ../../src/go/libs/ || exit 1
+gcc -o test test.c -lkipfsgo -L "$LIBDIR" -I "$LIBDIR" -I ../../src/go/libs/ || exit 1
 ./test
