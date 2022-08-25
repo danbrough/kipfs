756ad0519760cac906f3aeae27eb2ac3eed2c623
Dan Brough <dan@danbrough.org>
2022-08-25 16:52:35 +1200

    update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,7 @@
 object Dependencies {
   const val SONA_STAGING = "https://s01.oss.sonatype.org/content/groups/staging/"
+ // const val SONA_SNAPSHOTS = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+  
   const val klog = "org.danbrough:klog:_"
   const val coroutines_core = "org.danbrough.kotlinx:kotlinx-coroutines-core:_"
 }
--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -2,24 +2,28 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
   kotlin("multiplatform")
-  
+}
+
+repositories {
+  maven(Dependencies.SONA_STAGING)
+  //or for release version
+  //mavenCentral()
 }
 
 kotlin {
   linuxX64()
   macosX64()
   jvm()
+  
   sourceSets {
     val commonMain by getting {
       dependencies {
-        implementation(Dependencies.klog)
+        implementation("org.danbrough.kipfs:api:0.0.1-SNAPSHOT")
       }
     }
-    
-    
   }
   
-  val commonTest by sourceSets.getting{
+  val commonTest by sourceSets.getting {
     dependencies {
       implementation(kotlin("test"))
     }
@@ -27,10 +31,7 @@ kotlin {
   
   val nativeTest by sourceSets.creating {
     dependsOn(commonTest)
- 
   }
-  
-  
   
   targets.withType(KotlinNativeTarget::class).all {
     compilations["test"].apply {
