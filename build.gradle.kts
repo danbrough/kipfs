8fbb2a6ea251d6407be1d082e89da97ec6f2e187
Dan Brough <dan@danbrough.org>
2022-08-31 09:48:29 +1200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,6 @@ nexusPublishing {
     sonatype {
       nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
       snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-      stagingProfileId.set("orgdanbrough-1110")
     }
   }
 }
