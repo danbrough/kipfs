700870c0e5259b687fb9d7721dd2d4e733751ca0
Dan Brough <dan@danbrough.org>
2023-05-04 08:52:24 +1200

    update

---
+++ golib/src/go/kipfs/ble/bridge_unsupported.go
@@ -0,0 +1,22 @@
+//go:build !darwin && !android
+// +build !darwin,!android
+
+package ble
+
+import (
+	"go.uber.org/zap"
+
+
+	proximity "github.com/danbrough/kipfs/proximitytransport"
+)
+
+const Supported = false
+
+// Noop implementation for platform that are not Darwin
+
+func NewDriver(logger *zap.Logger) proximity.ProximityDriver {
+	logger = logger.Named("BLE")
+	logger.Info("NewDriver(): incompatible system")
+
+	return proximity.NewNoopProximityDriver(ProtocolCode, ProtocolName, DefaultAddr)
+}
